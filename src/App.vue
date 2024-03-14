a76bedb61df44aec3dd8b9ad768d3af13324368f
geraldofori <oforige@gmail.com>
2024-03-14 03:47:46 +0000

    rearranged the results component along with finishing css touches

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <h1>Gerald Reaction Game</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResult">Reaction time : {{score}} ms</p>
+  <Results v-if="showResult" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
 
 export default {
   name: 'App',
   components: {
-    Block
+    Block,
+    Results
   },
   data(){
     return{
@@ -45,5 +48,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  background: aqua;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
